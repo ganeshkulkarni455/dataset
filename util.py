e20fce8ef41423d8043596e5a437963255aded8a
ganeshkulkarni455 <64393872+ganeshkulkarni455@users.noreply.github.com>
2020-05-17 23:22:06 +0530

    Update and rename utils.py to util.py

--- util.py
+++ util.py
@@ -15,7 +15,7 @@ import torch.nn as nn
 import torchvision
 from tqdm import tqdm
 
-from . import torch_utils  # , google_utils
+from torch_util import *  # , google_utils
 
 matplotlib.rc('font', **{'size': 11})
 
